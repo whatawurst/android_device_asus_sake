1f915d606e373217648033f5f4692efa7bbf19d6
Andreas Schneider <asn@cryptomilk.org>
2021-06-07 16:08:11 +0200

    The big bang!
    Change-Id: Ia01b1df3118eec4bb75f0967589c1d12eef6fef7

---
+++ device.mk
@@ -0,0 +1,27 @@
+### PLATFORM
+$(call inherit-product, device/asus/sm8350-common/platform.mk)
+### PROPRIETARY VENDOR FILES
+$(call inherit-product, vendor/asus/sake/sake-vendor.mk)
+
+ifeq ($(WITH_FDROID),true)
+$(call inherit-product, vendor/fdroid/fdroid-vendor.mk)
+endif
+ifeq ($(WITH_MICROG),true)
+$(call inherit-product, vendor/microg/microg-vendor.mk)
+endif
+
+### DALVIK
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+DEVICE_PATH := device/asus/sake
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := 560dpi
+PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
+
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-lineage
+
+include $(DEVICE_PATH)/device/*.mk
