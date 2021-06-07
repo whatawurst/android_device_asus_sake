e7655cc0502d566d253e277705379ad1acb25321
Andreas Schneider <asn@cryptomilk.org>
2021-06-07 16:08:11 +0200

    The big bang!
    Change-Id: Ia01b1df3118eec4bb75f0967589c1d12eef6fef7

---
+++ BoardConfig.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+### INHERIT FROM YOSHIRO
+include device/asus/sm8350-common/BoardConfigPlatform.mk
+include vendor/asus/sake/BoardConfigVendor.mk
+
+DEVICE_PATH := device/asus/sake
+
+PRODUCT_PLATFORM := sm8350
+
+TARGET_OTA_ASSERT_DEVICE := sake,i006d
+
+### KERNEL
+ifeq ($(WITH_MAGISK),true)
+  TARGET_KERNEL_CONFIG := sm8350-sake_magisk_defconfig
+else
+  TARGET_KERNEL_CONFIG := sm8350-sake_defconfig
+endif
+
+### PARTITIONS
+# /proc/partitions shows the size in 1024-byte blocks
+BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296 # 98 MB
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824 # 24 MB
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 100663296 # 98 MB
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 243766472704 # 230GB
+
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+
+### DISPLAY
+TARGET_SCREEN_DENSITY := 440
+
+### PROPS
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
---
+++ lineage_sake.mk
@@ -0,0 +1,38 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Product API level
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_q.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/sake/device.mk)
+
+### BOOTANIMATION
+# vendor/lineage/config/common_full_phone.mk
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+# vendor/lineage/config/common.mk
+TARGET_BOOTANIMATION_HALF_RES := true
+
+### LINEAGE
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_sake
+PRODUCT_DEVICE := sake
+PRODUCT_BRAND := ASUS
+PRODUCT_MODEL := I006D
+PRODUCT_VERSION := 30.11.51.50
+PRODUCT_MANUFACTURER := asus
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=WW_$(PRODUCT_MODEL) \
+    PRODUCT_DEVICE=$(PRODUCT_BRAND)_$(PRODUCT_MODEL) \
+    PRODUCT_MODEL=$(PRODUCT_BRAND)_$(PRODUCT_MODEL)
+
+BUILD_FINGERPRINT := asus/WW_$(PRODUCT_MODEL)/$(PRODUCT_BRAND)_$(PRODUCT_MODEL):11/RKQ1.201022.002/$(PRODUCT_VERSION):user/release-keys
+
+ifneq ($(LINEAGE_DEV_CERTIFICATE),)
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(LINEAGE_DEV_CERTIFICATE)
+endif
